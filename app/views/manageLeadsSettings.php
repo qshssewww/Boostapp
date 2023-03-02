f6ce7d2a5d57a688f4e9a69cceafab96dae1aba5
qshssewww <T1A1arxFpzP$>
2023-03-02 18:33:07 +0300

    mb-20 ---> my-20 settings-icon

--- app/views/manageLeadsSettings.php
+++ app/views/manageLeadsSettings.php
@@ -1,5 +1,5 @@
 <link href="https://fonts.googleapis.com/css2?family=Rubik:ital,wght@0,400;0,500;1,400;1,500&display=swap" rel="stylesheet">
-<div class="mb-20 text-end">
+<div class="my-20 text-end">
     <a class="btn btn-outline-gray-300 text-black" href="javascript:;" onclick="LeadsSettings.openSettings();"><i class="fal fa-cog"></i></a>
     <div class="position-relative bsapp-drop-menu js-drop-menu">
         <div class=" shadow position-absolute  p-16  overflow-hidden bg-white rounded bsapp-z-999  w-100  text-start js-dropdown-inner bsapp-drop-menu-inner d-none">
